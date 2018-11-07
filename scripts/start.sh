efc18d6447afc264efbea0837bc4a81bab11dcc9
Rakshit Kumar <rakshit.kumar.cse17@itbhu.ac.in>
2018-11-08 00:57:15 +0530

    yo

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,18 +1,18 @@
 #TO INSTALL REQUIREMENTS
 sudo -H pip3 install -r $(pwd)/requirements.txt
 #TO REPLACE LOCAL SERVER SETTINGS
-echo 'Enter name to give to MySQL Database:'
+echo 'Enter name of MySQL Database:'
 read name
 echo 'Enter your MySQL username:'
 read user
 echo -n 'Enter your MySQL password:'
 read -s password
-sed -i "s|db|${name}|g" $(pwd)/website/settings.py
-sed -i "s|root|${user}|g" $(pwd)/website/settings.py
+sed -i "s|dbal|${name}|g" $(pwd)/website/settings.py
+sed -i "s|rootlove|${user}|g" $(pwd)/website/settings.py
 sed -i "s|blossom|${password}|g" $(pwd)/website/settings.py
-sudo python3 $(pwd)/manage.py makemigrations
-sudo python3 $(pwd)/manage.py migrate
-sudo python3 $(pwd)/manage.py runserver
+python3 $(pwd)/manage.py makemigrations
+python3 $(pwd)/manage.py migrate
+python3 $(pwd)/manage.py runserver
 
 
 
